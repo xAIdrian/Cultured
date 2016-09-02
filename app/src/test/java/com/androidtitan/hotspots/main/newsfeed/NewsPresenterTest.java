405d4acde84695963ca06385020b27ba6de495f9
Adrian Mohnacs <amohnacss@gmail.com>
2016-09-01 21:51:28 -0400

    grade configuration fixes for testing

---
+++ app/src/test/java/com/androidtitan/hotspots/main/newsfeed/NewsPresenterTest.java
@@ -0,0 +1,30 @@
+package com.androidtitan.hotspots.main.newsfeed;
+
+import com.androidtitan.hotspots.BuildConfig;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+
+/**
+ * Created by amohnacs on 9/1/16.
+ */
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class NewsPresenterTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
---
+++ app/src/test/java/newsfeed/NewsProviderTest.java
@@ -0,0 +1,31 @@
+package newsfeed;
+
+import com.androidtitan.hotspots.BuildConfig;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+/**
+ * Created by amohnacs on 9/1/16.
+ */
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class NewsProviderTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void sampleTest() throws Exception {
+
+    }
+
+
+}
