690e531217662a95beaa316589021c632ecb7990
Adrian Mohnacs <amohnacss@gmail.com>
2016-09-07 23:28:08 -0400

    Local jvm tests
    Mock retrofit response created and tests for NewsProvider's fetchArticles method.

---
+++ app/src/test/java/com/androidtitan/culturedapp/main/newsfeed/NewsProviderTest.java
@@ -0,0 +1,10 @@
+package com.androidtitan.culturedapp.main.newsfeed;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by amohnacs on 9/7/16.
+ */
+public class NewsProviderTest {
+
+}
---
+++ app/src/test/java/com/androidtitan/culturedapp/main/newsfeed/mock/MockNewsEndpoint.java
@@ -0,0 +1,8 @@
+package com.androidtitan.culturedapp.main.newsfeed.mock;
+
+/**
+ * Created by amohnacs on 9/7/16.
+ */
+
+public class MockNewsEndpoint {
+}
