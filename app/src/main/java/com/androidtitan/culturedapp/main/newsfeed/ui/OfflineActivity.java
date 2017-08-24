4a0c57eeedd1dc5b1c9cfa15ca8ab5214ced8ae0
Adrian Mohnacs <amohnacss@gmail.com>
2017-08-23 22:43:07 -0400

    Stable build and proper xml for OfflineActivity

--- app/src/main/java/com/androidtitan/culturedapp/main/newsfeed/ui/OfflineActivity.java
+++ app/src/main/java/com/androidtitan/culturedapp/main/newsfeed/ui/OfflineActivity.java
@@ -5,18 +5,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.androidtitan.culturedapp.R;
+import com.androidtitan.culturedapp.common.FileManager;
 import com.androidtitan.culturedapp.databinding.OfflineActivityBinding;
+import com.androidtitan.culturedapp.model.newyorktimes.Article;
+
+import java.util.ArrayList;
 
 public class OfflineActivity extends AppCompatActivity {
 
-    //todo: get our list of offline articles from {@link FileManager} and then tie it to our data binding
+    private FileManager fileManager;
+
+    private ArrayList<Article> offlineArticles;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.offline_activity);
+
+        fileManager = FileManager.getInstance(this);
+
+        offlineArticles = fileManager.getInternalArticles();
+
         OfflineActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.offline_activity);
-        //binding.setArticle_one();
-        //binding.setArticle_two();
-        //binding.setArticle_three();
+        //OfflineActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.offline_activity);
+
+        binding.setArticle_one(offlineArticles.get(0));
+        binding.setArticle_two(offlineArticles.get(1));
+        binding.setArticle_three(offlineArticles.get(2));
+
     }
 }
